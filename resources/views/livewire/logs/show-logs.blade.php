5b404797e585fa8d246662efaee3342a4aa323c4
Abdelrhman Mharan <86526333+abdoMoharan@users.noreply.github.com>
2023-12-27 20:49:19 +0200

    update them

--- resources/views/livewire/logs/show-logs.blade.php
+++ resources/views/livewire/logs/show-logs.blade.php
@@ -31,9 +31,9 @@
                                                         @endphp
 
                                                         <a class="list-group-item list-group-item-action border-0" wire:click.prevent="getMessges('{{ $twin_id }}', '{{ $botpress_conversation_id }}')">
-                                                            <div class="badge bg-success float-right">{{count($messages)}}</div>
                                                             <div class="d-flex align-items-start">
-                                                                <img src="https://bootdey.com/img/Content/avatar/avatar5.png" class="rounded-circle mr-1" alt="Vanessa Tucker" width="40" height="40">
+                                                                <div class="badge bg-success float-right">{{count($messages)}}</div>
+
                                                                 <div class="flex-grow-1 ml-3">
                                                                     @if($messages->isNotEmpty())
                                                                         <div class="small">{{ $messages->first()->created_at->format('Y-m-d H:i:s') }}</div>
@@ -54,14 +54,8 @@
                                                     <div class="col-12 col-lg-7 col-xl-9">
                                                         <div class="py-2 px-4 border-bottom d-none d-lg-block">
                                                             <div class="d-flex align-items-center py-1">
-                                                                <div class="position-relative">
-                                                                    <img src="https://bootdey.com/img/Content/avatar/avatar3.png" class="rounded-circle mr-1" alt="Sharon Lessman" width="40" height="40">
-                                                                </div>
-                                                                <div class="flex-grow-1 pl-3">
-                                                                    {{-- {{ $this->model->title }}{ --}}
-                                                                    <strong></strong>
-                                                                    {{-- <div class="text-muted small"><em>Typing...</em> --}}
-                                                                </div>
+
+
                                                                 {{-- <div>
                                                                     <button class="btn btn-primary btn-lg mr-1 px-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone feather-lg">
                                                                             <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
