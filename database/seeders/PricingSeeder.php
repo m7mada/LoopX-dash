af643cd663ed9043721b1555a119a9994c15763d
Mohamed Hamada <m.hamada@360codes.net>
2024-01-14 03:43:33 +0200

    add package seeds , fix list messages

---
+++ database/seeders/PricingSeeder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Package;
+use App\Models\PackagesBenefit;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Country ;
+use App\Models\Currency ;
+use App\Models\PackagesPrice ;
+use App\Models\Benefit ;
+
+
+class PricingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Country::updateOrInsert([
+            'name' => "Egypt",
+            "code"=> "eg",
+        ]);
+
+        Currency::updateOrInsert([
+            "code"=> "EGP",
+        ]) ;
+
+        PackagesPrice::insert([
+            "price"=>2000,
+            "currency_id"=>Currency::where('code','EGP')->first()->id,
+            "country_id"=>Country::where('code','eg')->first()->id,
+            "package_id"=>Package::first()->id,
+        ]);
+
+        Benefit::insert([
+            [
+                "name"=> "Monthly Subscription",
+                "type"=> "duration",
+            ],
+            [
+                "name"=> "Credits",
+                "type"=> "cridet",
+            ],
+            [
+                "name"=> "Support",
+                "type"=> "support",
+            ]
+            ]
+        );
+
+        PackagesBenefit::insert([
+            [
+                "package_id"=>Package::first()->id,
+                "benefit_id"=>Benefit::where('type','duration')->first()->id,
+                "value"=>1,
+            ],
+            [
+                "package_id"=>Package::first()->id,
+                "benefit_id"=>Benefit::where('type','cridet')->first()->id,
+                "value"=>5,
+            ],
+            [
+                "package_id"=>Package::first()->id,
+                "benefit_id"=>Benefit::where('type','support')->first()->id,
+                "value"=>2,
+            ]
+            ]
+        );
+
+    }
+}
--- resources/views/livewire/twins/list.blade.php
+++ resources/views/livewire/twins/list.blade.php
@@ -62,7 +62,7 @@
                                             <td class="align-middle text-center">
 
                                                 <div class="d-flex align-items-center justify-content-center">
-                                                    <span class="me-2 text-xs font-weight-bold">{{count($twin->messages)}} Messages </span>
+                                                    <span class="me-2 text-xs font-weight-bold">{{count($twin->messages->where('role','=','assistant'))}} Reply </span>
                                                     <!-- <div>
                                                         <div class="progress">
                                                             <div class="progress-bar bg-gradient-info"
