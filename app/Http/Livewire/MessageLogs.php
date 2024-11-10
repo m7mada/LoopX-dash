b685a72e6329c9790191ab31f80f43026aac8b99
Mohamed Hamada <m.hamada@360codes.net>
2024-11-10 22:57:08 +0200

    fix lang

--- app/Http/Livewire/MessageLogs.php
+++ app/Http/Livewire/MessageLogs.php
@@ -132,7 +132,7 @@ class MessageLogs extends Component
                                     ->get();
 
     
-       dd($this->model->messages->limit(10));
+       //dd($this->model->messages->last());
         //$this->dispatchBrowserEvent('selectConversation');
 
     }
