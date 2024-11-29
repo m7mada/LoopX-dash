6aac79d5d36a1b69982f291add66f11a9d2b9e16
Mohamed Hamada <m.hamada@360codes.net>
2024-11-29 09:00:12 +0200

    show whatsapp data t the messages list

--- resources/views/livewire/logs/messages.blade.php
+++ resources/views/livewire/logs/messages.blade.php
@@ -120,13 +120,9 @@
                                                                                 </div> -->
 
                                                                                 <div class="small" style="text-transform: capitalize;">
-                                                                                @if( is_array( $messages->last()->botpress_userData ))
-                                                                                    @forelse($messages->last()->botpress_userData as $key=>$value)
-                                                                                        {{ $key }} : {{$value}} | 
-                                                                                    @empty
-                                                                                        {{ $messages->first()->botpress_integration }} User 
-                                                                                    @endforelse
-                                                                                @endif
+
+                                                                                    {{ $messages->last()->botpress_userData['fullName'] ?? $messages->first()->botpress_integration . "User" }}
+                                                                                
                                                                                 </div>
                                                                                 <div class="small" style="font-size: 10px;">
                                                                                     <span class="fas fa-circle @if ( empty($messages->first()->isPauseConversation) )chat-online @else text-danger @endif"></span>
@@ -136,19 +132,7 @@
                                                                                     id="botpress_conversation_id_{{$conversationId ?? '0'}}">
                                                                                     {{$conversationId ?? "0"}}
                                                                                         <i class="fa fa-clone" aria-hidden="true"></i>
-                                                                                    </span>
-                                                                                    {{--
-                                                                                    <div class="small">
-
-                                                                                        @if( is_array( $messages->last()->botpress_userData ))
-                                                                                            @forelse($messages->last()->botpress_userData as $key=>$value)
-                                                                                                {{ $key }} : {{$value}} <br>
-                                                                                            @empty
-                                                                                            -
-                                                                                            @endforelse
-                                                                                        @endif
-                                                                                    </div>
-                                                                                    --}}
+                                                                            </span>
                                                                                     <div class="small" style="font-size: 11px;display: flex;flex-direction: column;justify-content: center;align-items: self-start;margin-top: 5px;">
                                                                                         <span>{{ $messages->last()->created_at->format('M j, y g:iA') }}</span>
                                                                                     </div>
@@ -193,7 +177,6 @@
                                                                             @endif
                                                                         </div>
 
-
                                                                         <div class="d-flex flex-row align-items-center">
                                                                             <button wire:click="playPauseConversation('{{ $this->mt_twins->first()->botpress_conversation_id }}')"
                                                                                 type="button" style="
@@ -219,25 +202,7 @@
                                                                             @endif
                                                                         </div>
                                                                     @endif
-                                                                    
-                                                                    
-                                                                    {{--<div>
-                                                                        <button class="btn btn-primary btn-lg mr-1 px-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone feather-lg">
-                                                                                <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
-                                                                                </path>
-                                                                            </svg></button>
-                                                                        <button class="btn btn-info btn-lg mr-1 px-3 d-none d-md-inline-block"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-video feather-lg">
-                                                                                <polygon points="23 7 16 12 23 17 23 7">
-                                                                                </polygon>
-                                                                                <rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect>
-                                                                            </svg></button>
-                                                                        <button class="btn btn-light border btn-lg px-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal feather-lg">
-                                                                                <circle cx="12" cy="12" r="1"></circle>
-                                                                                <circle cx="19" cy="12" r="1"></circle>
-                                                                                <circle cx="5" cy="12" r="1"></circle>
-                                                                            </svg></button>
-                                                                    </div>
-                                                                    --}}
+                            
                                                                 </div>
                                                             </div>
 
@@ -273,7 +238,7 @@
                                                                     </div>
                                                                     @endif
                                                                     @empty
-                                                                    <span>No Messages Selected!</span>
+                                                                    <span>No Messages Selected !</span>
                                                                     @endforelse
                                                                     @endif 
 
