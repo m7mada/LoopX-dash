e89cb19834494f797aa8e1ee44033056295c4150
Mohamed Hamada <m.hamada@360codes.net>
2024-12-31 22:35:36 +0200

    reporting controller with customer wallet

--- app/Http/Controllers/ReportingController.php
+++ app/Http/Controllers/ReportingController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Twin;
-use App\Models\Messages;
-use App\Models\Order;
-use MongoDB\BSON\ObjectId;
-use Carbon\Carbon;
+// use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+// use App\Models\Twin;
+// use App\Models\Messages;
+// use App\Models\Order;
+// use MongoDB\BSON\ObjectId;
+// use Carbon\Carbon;
 use DB;
 
 class ReportingController extends Controller
 {
     public function customersWallet(){
+
+        if( ! Auth::user()->is_admin ){
+            return "";
+        }
         $totalCridets = DB::select("SELECT SUM(order_lines.value) as total_credits FROM order_lines WHERE order_lines.order_id IN ( SELECT id FROM orders WHERE orders.is_paid = 1 AND order_lines.benefit_id = ( SELECT id FROM benefits WHERE benefits.type = 'cridet' ))");
 
         foreach( $totalCridets as $customerCridet ){
