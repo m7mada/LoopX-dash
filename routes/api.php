203f8e1fc50beba03392555d152e467ddc3d6f03
Mohamed Hamada <m.hamada@360codes.net>
2025-01-27 20:46:01 +0200

    add new proxy functon

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,10 @@ Route::middleware('auth:twins')->group(function () {
     Route::post('/v1/conversation/messages', [ThirdPartyApiController::class, 'listConversationMessages']);
     Route::post('/v1/message/send', [ThirdPartyApiController::class, 'sendMessage']);
     //Route::any('/{endpoint}', [ThirdPartyApiController::class, 'proxy'])->where('endpoint', '.+');
-    Route::any('/proxyFBAppTobootPress', [ThirdPartyApiController::class, 'proxyFBAppTobootPress']);
 
 
 });
+Route::any('/proxyFBAppTobootPress', [ThirdPartyApiController::class, 'proxyFBAppTobootPress']);
 
 
 Route::post('/recive_messages', [ThirdPartyApiController::class, 'reciveMessages']);
