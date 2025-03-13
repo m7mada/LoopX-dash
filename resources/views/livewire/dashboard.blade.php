f4250a42644456dd3190d9d79fa24373218f5089
root <root@ip-172-31-24-135.ec2.internal>
2025-03-13 12:09:05 +0000

    server u

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -5,7 +5,31 @@
 
           <div class="row">
 
-          
+            @if(\Auth::user()->is_admin)
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card">
+                        <div class="card-header p-3 pt-2">
+                            <div
+                                class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                                <i class="material-icons opacity-10">weekend</i>
+                            </div>
+                            <div class="text-end pt-1">
+                                <p class="text-sm mb-0 text-capitalize">Remaining Customers Wallet</p>
+                                <h4 class="mb-0">{{$customersRemainingCridets}}</h4>
+                            </div>
+                        </div>
+                        <hr class="dark horizontal my-0">
+                        <div class="card-footer p-3">
+                            <p class="mb-0"><span class="text-success text-sm font-weight-bolder">{{$this->customersTotalMessages}} </span>
+                                Total Messages </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row"><br></div>
+
+              @endif
+
               <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
                   <div class="card">
                       <div class="card-header p-3 pt-2">
