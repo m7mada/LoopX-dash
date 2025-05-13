e3022b0b734d09d88af060b67935d7d687db5c47
Mohamed Hamada <mohjamed.7mada@gmail.com>
2025-05-13 15:25:15 +0300

    save fb page access token to twin

--- app/Models/Twin.php
+++ app/Models/Twin.php
@@ -14,7 +14,7 @@ class Twin extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'title', 'user_id', 'is_active','agent_persona','agent_instructions','example_messagesa','kb_model_name','msgs_model_name','agent_dialect','user_dialect','is_active','twin_external_id','creativity_temperature','botbress_bot_id','integrations_settings','custom_settings','handover_settings','botbress_bot_id','botbress_integration_key','botbress_workspace_id','botpress_webhook_link','botpress_access_token','fb_page_id'
+        'title', 'user_id', 'is_active','agent_persona','agent_instructions','example_messagesa','kb_model_name','msgs_model_name','agent_dialect','user_dialect','is_active','twin_external_id','creativity_temperature','botbress_bot_id','integrations_settings','custom_settings','handover_settings','botbress_bot_id','botbress_integration_key','botbress_workspace_id','botpress_webhook_link','botpress_access_token','fb_page_id','fb_page_access_token'
     ];
     public $timestamps = true;
 
