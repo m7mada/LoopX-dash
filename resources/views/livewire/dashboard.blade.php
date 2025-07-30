a68395d666be3a3a794ebb043fb80aed0dd02cc9
Mohamed Hamada <mohjamed.7mada@gmail.com>
2025-07-30 19:36:29 +0300

    calculate wallet

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -15,10 +15,10 @@
                             </div>
                             <div class="text-end pt-1">
                                 <p class="text-sm mb-0 text-capitalize">Remaining Customers Wallet</p>
-                                <h4 class="mb-0">{{$this->customersRemainingCridets}} including %65</h4>
-                                <h4 class="mb-0">{{$this->totalCustomersUsed}} Total Customres usage with %65</h4>
-                                <h4 class="mb-0">{{$this->customersTotalCridets[0]->total_credits}} Total Customres Paid</h4>
-                                <h4 class="mb-0">{{$this->customersTotalCridets[0]->total_credits + $this->customersTotalCridets[0]->total_credits * 0.35 }} Expected Customres Cost</h4>
+                                <p class="mb-0">{{$this->customersRemainingCridets}} including %65</p>
+                                <p class="mb-0">{{$this->totalCustomersUsed}} Total Customres usage with %65</p>
+                                <p class="mb-0">{{$this->customersTotalCridets[0]->total_credits}} Total Customres Paid</p>
+                                <p class="mb-0">{{$this->customersTotalCridets[0]->total_credits + $this->customersTotalCridets[0]->total_credits * 0.35 }} Expected Customres Cost</p>
 
                                 
                             </div>
