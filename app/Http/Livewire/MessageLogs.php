55dec0af9cf7e970fcfce1fed8feb61bf5903110
Mohamed Hamada <mohjamed.7mada@gmail.com>
2025-08-24 10:08:13 +0300

    disable channel

--- app/Http/Livewire/MessageLogs.php
+++ app/Http/Livewire/MessageLogs.php
@@ -39,7 +39,7 @@ class MessageLogs extends Component
                 if( request()->search_conversation_id ){
                    $query->where('botpress_conversation_id', '=', request()->search_conversation_id);
                 }
-                $query->where('botpress_channel', '=', 'channel');
+                // $query->where('botpress_channel', '=', 'channel');
 
                 if(request()->search_date_from || request()->search_date_to){
                     $startDate = request()->has('search_date_from') ? Carbon::parse(request()->search_date_from) : now()->subMonth();;
@@ -87,7 +87,7 @@ class MessageLogs extends Component
         $this->model = Twin::where('twin_external_id',$twin_id)
             ->with("messages", function ($query) {
                 $query->orderBy('created_at', 'asc');
-                $query->where('botpress_channel', '=', 'channel');
+                // $query->where('botpress_channel', '=', 'channel');
 
                 if ($this->filters['search_conversation_id']) {
                     $query->where('botpress_conversation_id', '=', $this->filters['search_conversation_id']);
