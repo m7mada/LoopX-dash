4d0baf738742c7ce9fe58c9cc20cdf5f34b5011d
Mohamed Hamada <mohjamed.7mada@gmail.com>
2025-08-27 20:14:27 +0300

    fix nun user model

--- app/Http/Livewire/MessageLogs.php
+++ app/Http/Livewire/MessageLogs.php
@@ -65,7 +65,7 @@ class MessageLogs extends Component
             ->first();
 
         if( !$this->model ){
-            return abort(404);
+            return redirect()->route('twins.index') ->with('error', 'Twin not found or you do not have permission to access it.');
         }
 
         //dd($this->model->messages->limit(10));
