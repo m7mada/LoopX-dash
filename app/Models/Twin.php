341740389ae10f6968c1999e21cbe4408dd7847b
EngMahmoudElgml <eng.m.ahmed99@gmail.com>
2025-11-18 10:16:00 +0200

    add fields to twin fillable

--- app/Models/Twin.php
+++ app/Models/Twin.php
@@ -5,16 +5,45 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use App\Models\File;
-use App\Models\Messages;
-use App\Models\User;
 
 
 class Twin extends Model
 {
-    use HasFactory;
     protected $fillable = [
-        'title', 'user_id', 'is_active','agent_persona','agent_instructions','example_messagesa','kb_model_name','msgs_model_name','agent_dialect','user_dialect','is_active','twin_external_id','creativity_temperature','botbress_bot_id','integrations_settings','custom_settings','handover_settings','botbress_bot_id','botbress_integration_key','botbress_workspace_id','botpress_webhook_link','botpress_access_token','fb_page_id','fb_page_access_token'
+        'title',
+        'user_id',
+        'is_active',
+        'agent_persona',
+        'agent_instructions',
+        'example_messagesa',
+        'kb_model_name',
+        'msgs_model_name',
+        'agent_dialect',
+        'user_dialect',
+        'is_active',
+        'twin_external_id',
+        'creativity_temperature',
+        'botbress_bot_id',
+        'integrations_settings',
+        'custom_settings',
+        'handover_settings',
+        'botbress_bot_id',
+        'botbress_integration_key',
+        'botbress_workspace_id',
+        'botpress_webhook_link',
+        'botpress_access_token',
+        'fb_page_id',
+        'fb_page_access_token',
+        'routing_agent_instructions',
+        'rag_agent_instructions',
+        'copilot_settings',
+        'fb_page_id',
+        'wa_phone_number',
+        'wa_phone_number_id',
+        'fb_webhook_proxy_url',
+        'botpress_chat_webhook_id',
+        'wa_webhook_proxy_url',
+        'fb_page_access_token'
     ];
     public $timestamps = true;
 
@@ -23,7 +52,7 @@ class Twin extends Model
         return $this->hasMany(File::class);
     }
 
-    public function messages(): HasMany 
+    public function messages(): HasMany
     {
         // $instance = $this->newRelatedInstance(Messages::class);
 
